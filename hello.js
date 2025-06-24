66f8591a03afc7dc5d71a39b60eb9312b3233fb3
122168 <36868357+122168@users.noreply.github.com>
2025-06-24 13:40:10 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,8 @@
-export function sayHello() {
-  console.log("✅ ES Module loaded successfully!");
+//export function sayHello() {
+ // console.log("✅ ES Module loaded successfully!");
+//}
+
+// math.js
+export function add(a, b) {
+  return a + b;
 }
